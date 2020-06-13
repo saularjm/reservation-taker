ec249992a8e12753bfd1297e21a14dffbe0c91c5
Sandra <sandra.alpha2010@gmail.com>
2020-06-13 11:55:57 -0700

    route html

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
 
-
+//Basic route that sends the user first to the AJAX page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+//route to tables html when tables button is pressed
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+//route to reserve html when reserve table button is pressed
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
