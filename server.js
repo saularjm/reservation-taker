c4b6b04033a1beb1f745a7e859d9b5d002ba96fe
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-06-13 11:51:50 -0700

    created JSON get/post routes

--- server.js
+++ server.js
@@ -12,7 +12,54 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
+// DATA
+let data = {
+    reservations = [
+        {
+            name: "test name",
+            phone: "555-867-5309",
+            email: "test@test.com",
+            uniqueID: this.name
+        }
+    ],
+    waitList = [
+        {
+            name: "test name2",
+            phone: "555-555-5555",
+            email: "wait@wait.com",
+            uniqueID: this.name
+        }
+    ]
+};
 
+// Route for table data JSON
+app.get("/api/tables", function(req, res) {
+    return res.json(data);
+  });
+
+// Route for waitlist JSON
+app.get("/api/waitlist", function(req, res) {
+    return res.json(data.waitList);
+});
+
+// Route for reservation JSON
+app.get("/api/reservations", function(req, res) {
+    return res.json(data.reservations);
+});
+
+// Route to post new reservation
+app.post("/api/tables", function(req, res) {
+    let newRes = req.body;
+    console.log(newRes);
+  
+    if (data.reservations.length < 5) {
+        data.reservations.push(newRes);
+    } else {
+        data.waitList.push(newRes);
+    }
+  
+    res.json(data);
+  });
 
 
 // Starts the server to begin listening
